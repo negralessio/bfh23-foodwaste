f0f80a193d493a9f408882f458b02ba72315409a
el-presidente-kukki <armbruster.marius@t-online.de>
2023-10-14 10:51:55 +0200

    Added inital Design - Bug Fixes Needed

--- app/homegui.py
+++ app/homegui.py
@@ -1,8 +1,9 @@
 import streamlit as stm
 from streamlit_extras.app_logo import add_logo
 
-# Hide the default footer and set Basic Settings
-stm.set_page_config(page_title="Edeka Suedwest", page_icon="🍎", layout="wide")
+stm.set_page_config(page_title="EDEKA - Wir lieben Lebensmittel", page_icon="assets/favicon.png", layout="wide")
+
+#Custom CSS
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -10,9 +11,21 @@ hide_streamlit_style = """
             </style>
             """
 stm.markdown(hide_streamlit_style, unsafe_allow_html=True)
+background_image_style = """
+            <style>
+            .st-emotion-cache-6qob1r {
+                background-image: url("https://www.edeka.de/edeka-ui-assets/images/bg-dark.jpg");
+                background-size: cover;
+            }
+            </style>
+            """
+stm.markdown(background_image_style, unsafe_allow_html=True)
 
 # Configure the sidebar
-stm.sidebar.title("Navigation")
+with stm.sidebar:
+    stm.image("assets/claim-grunge-650.png", use_column_width=True)
+    stm.text("Made with ❤️ by DataPilots")
 add_logo("assets/edeka_logo.png", height=200)
+
 
 stm.title("This is the Home Page")
--- app/pages/pageone.py
+++ app/pages/pageone.py
@@ -1,4 +1,31 @@
 import streamlit as stm
+from streamlit_extras.app_logo import add_logo
+
+stm.set_page_config(page_title="EDEKA - Wir lieben Lebensmittel", page_icon="assets/favicon.png", layout="wide")
+
+#Custom CSS
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+stm.markdown(hide_streamlit_style, unsafe_allow_html=True)
+background_image_style = """
+            <style>
+            .st-emotion-cache-6qob1r {
+                background-image: url("https://www.edeka.de/edeka-ui-assets/images/bg-dark.jpg");
+                background-size: cover;
+            }
+            </style>
+            """
+stm.markdown(background_image_style, unsafe_allow_html=True)
+
+# Configure the sidebar
+with stm.sidebar:
+    stm.image("assets/claim-grunge-650.png", use_column_width=True)
+    stm.text("Made with ❤️ by DataPilots")
+add_logo("assets/edeka_logo.png", height=200)
 
 stm.title("This is PageOne Geeks.")
 stm.sidebar.title("Navigation 2")
