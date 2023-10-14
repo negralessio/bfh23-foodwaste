010dcaabdcc311e7978c9bce698fc2d9fd342829
el-presidente-kukki <armbruster.marius@t-online.de>
2023-10-14 10:11:03 +0200

    Added new Edeka Logo and configured Streamlit and Base Colors

--- app/homegui.py
+++ app/homegui.py
@@ -1,5 +1,18 @@
 import streamlit as stm
+from streamlit_extras.app_logo import add_logo
 
+# Hide the default footer and set Basic Settings
 stm.set_page_config(page_title="Edeka Suedwest", page_icon="🍎", layout="wide")
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+stm.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# Configure the sidebar
+stm.sidebar.title("Navigation")
+add_logo("assets/edeka_logo.png", height=200)
+
 stm.title("This is the Home Page")
-stm.sidebar.success("Select Any Page from here")
--- app/pages/pageone.py
+++ app/pages/pageone.py
@@ -1,3 +1,4 @@
 import streamlit as stm
 
 stm.title("This is PageOne Geeks.")
+stm.sidebar.title("Navigation 2")
